1f43fbf4d048f30030ffbe9df9b318c8feb0d5f4
法師人大基 <pop.maker0408@gmail.com>
2020-09-11 00:44:02 +0900

    dist対応

--- dist/GI_Project.e31bb0bc.js
+++ dist/GI_Project.e31bb0bc.js
@@ -28487,7 +28487,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62015" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50134" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -28664,4 +28664,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/GI_Project.e31bb0bc.js.map
+//# sourceMappingURL=/GOLIKUMOU/dist/GI_Project.e31bb0bc.js.map
